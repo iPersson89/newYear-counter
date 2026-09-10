586d86c5e850fe9c654c00b28433572ba118b1a8
Ida <ida@testmail.com>
2026-09-10 21:06:57 +0200

    fixed bug

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ const y = new Date('2026-12-31');
 let numberOfDays = Math.floor((y - x) / (1000 * 60 * 60 * 24));
 const namn = "ida" 
 
-documentFragment.getElementById("newYearCounter").textContent = console.log(`YO! ${namn.toUpperCase()}, det är ${numberOfDays} dagar kvar till nyår!`);
+document.getElementById("newYearCounter").textContent = (`YO! ${namn.toUpperCase()}, det är ${numberOfDays} dagar kvar till nyår!`);
 
