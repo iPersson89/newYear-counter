d6112d56501dfebc779de72550bef44606802443
Ida <ida@testmail.com>
2026-09-10 20:50:28 +0200

    Kopplat JS till HTML

--- main.js
+++ main.js
@@ -3,5 +3,5 @@ const y = new Date('2026-12-31');
 let numberOfDays = Math.floor((y - x) / (1000 * 60 * 60 * 24));
 const namn = "ida" 
 
-console.log(`YO! ${namn.toUpperCase()}, det är ${numberOfDays} dagar kvar till nyår!`);
+documentFragment.getElementById("newYearCounter").textContent = console.log(`YO! ${namn.toUpperCase()}, det är ${numberOfDays} dagar kvar till nyår!`);
 
